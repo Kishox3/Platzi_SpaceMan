cf7e2af3f58c92c7e6e14a97efeebc6d54486a7e
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2023-05-04 19:28:58 -0600

    Class 9 & Class 10
    We create the jump method and script, but now with space jump to infinity Y

--- SpaceMan_Game/Assets/Scripts/PlayerController.cs
+++ SpaceMan_Game/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     //Variables del movimiento del personaje
     public float jumpForce = 6f; //float
-    private Rigidbody2D playerRigidBody; //no colocar nada hace que la variable sea private
+    private Rigidbody2D rb; //no colocar nada hace que la variable sea private
+
+    
+    void Awake()
+    {
+        rb = GetComponent <Rigidbody2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +23,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
     }
 
     void Jump()
     {
-
+        rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
     }
 }
