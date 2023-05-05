f2b43fa08ba6f1e9a204ca7a6d5b273f1da6998e
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2023-05-04 22:18:14 -0600

    Class 11 and 12 - Debug and animations
    We add the debug/gizmo ray and also we pause the animation when the character is jumping, and change the animation when he jump

--- SpaceMan_Game/Assets/Scripts/PlayerController.cs
+++ SpaceMan_Game/Assets/Scripts/PlayerController.cs
@@ -4,48 +4,75 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /* -- variables -- */
+
+    //public
     public float jumpForce = 6f;
-    private Rigidbody2D rb; 
     public LayerMask groundMask;
-    
+    //private
+    private Rigidbody2D rb; 
+    private Animator animator;
+    //constant
+    private const string STATE_ALIVE = "isAlive";                   
+    private const string STATE_ON_THE_GROUND = "isOnTheGround";     
+
+
+    /* -- core methods -- */
+
+    //inicializa cuando se abre el juego
+    void Start(){
+        animator.SetBool(STATE_ALIVE, true);
+        animator.SetBool(STATE_ON_THE_GROUND, true);
+    }
+
+    //inicializa cuando se activa el script
     void Awake()
     {
-        rb = GetComponent <Rigidbody2D>();
+   
+        rb = GetComponent<Rigidbody2D>();           //se obtiene componente del rigidbody del personaje
+        animator = GetComponent<Animator>();        //se obtiene componente del animator del personaje
     }
 
-    void Start()
-    {
+    //Reconoce acciones por cada fotograma
+    void Update() { 
         
-    }
-
-    void Update() //Reconoce acciones por cada fotograma
-    {
         if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Jump();
         }
+
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());               //se le da un valor a una variable desde lo que devuleve el metodo
+
+        // -- gizmos  --
+        Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
     }
 
-    void Jump() //Hace la accion de saltar
+
+    /* -- custom methods -- */
+
+    //Indica si el personaje esta o no tocando el suelo
+    bool IsTouchingTheGround()
     {
-        if (IsTouchingTheGround())
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask))
         {
+            //ToDo: programar logica de contacto
+            animator.enabled = true;
+            return true;
+        }
+        else
+        {
+            //ToDo: programar logica de no contacto
+            animator.enabled = false;   //se quedara con el ultimo frame de salto
+            return false;
+        }
+    }
+
+    //Hace que el persona salte si esta tocando el suelo
+    void Jump(){
+        if (IsTouchingTheGround()){
             rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
         }
     }
 
-    
-    bool IsTouchingTheGround() //Indica si el personaje esta o no tocando el suelo
-    {
-        if (Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask))
-        {
-            //TODO: programar logica de contacto
-            return true;
-        }
-        else 
-        {
-            //TODO: programar logica de no contacto
-            return false;
-        }
-    }
+
 }
