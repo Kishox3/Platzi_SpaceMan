5bfa5c3e346146f71cf1909242cfecd3325e752f
Eduardo Peraza <eduardo.peraza.vindas@gmail.com>
2023-05-06 10:31:14 -0600

    Class 12 - Cambiar las animaciones según los estados
    This class was a challenge, I create a blend tree and only use 2 sprites for the animation to look more natural
    I utilize the velocityY value of the rigidbody2D for the transitions on the blend

--- SpaceMan_Game/Assets/Scripts/PlayerController.cs
+++ SpaceMan_Game/Assets/Scripts/PlayerController.cs
@@ -7,20 +7,22 @@ public class PlayerController : MonoBehaviour
     /* -- variables -- */
 
     //public
-    public float jumpForce = 6f;
+    public float jumpForce = 8f;
     public LayerMask groundMask;
+    public float velocityY;
     //private
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
     private Animator animator;
     //constant
-    private const string STATE_ALIVE = "isAlive";                   
-    private const string STATE_ON_THE_GROUND = "isOnTheGround";     
+    private const string STATE_ALIVE = "isAlive";
+    private const string STATE_ON_THE_GROUND = "isOnTheGround";
 
 
     /* -- core methods -- */
 
     //inicializa cuando se abre el juego
-    void Start(){
+    void Start()
+    {
         animator.SetBool(STATE_ALIVE, true);
         animator.SetBool(STATE_ON_THE_GROUND, true);
     }
@@ -28,20 +30,31 @@ public class PlayerController : MonoBehaviour
     //inicializa cuando se activa el script
     void Awake()
     {
-   
+
         rb = GetComponent<Rigidbody2D>();           //se obtiene componente del rigidbody del personaje
         animator = GetComponent<Animator>();        //se obtiene componente del animator del personaje
+
     }
 
     //Reconoce acciones por cada fotograma
-    void Update() { 
-        
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Jump();
         }
 
         animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());               //se le da un valor a una variable desde lo que devuleve el metodo
+
+        float velocityY = rb.velocity.y;
+        animator.SetFloat("velocityY", velocityY);
+
+        // Detecta si el jugador ya no está saltando
+        if (!IsTouchingTheGround() && velocityY <= 0.1f)
+        {
+            animator.SetFloat("velocityY", 0f);
+        }
 
         // -- gizmos  --
         Debug.DrawRay(this.transform.position, Vector2.down*1.5f, Color.red);
@@ -56,23 +69,26 @@ public class PlayerController : MonoBehaviour
         if (Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask))
         {
             //ToDo: programar logica de contacto
-            animator.enabled = true;
             return true;
         }
         else
         {
             //ToDo: programar logica de no contacto
-            animator.enabled = false;   //se quedara con el ultimo frame de salto
             return false;
         }
     }
 
     //Hace que el persona salte si esta tocando el suelo
-    void Jump(){
-        if (IsTouchingTheGround()){
+    void Jump()
+    {
+        if (IsTouchingTheGround())
+        {
             rb.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
         }
+        float velocityY = rb.velocity.y;
+        animator.SetFloat("velocityY", velocityY);
     }
+
 
 
 }
